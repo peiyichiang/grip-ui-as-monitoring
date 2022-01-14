66c7efafd1453275f5da65677806bd6543599193
Alberto Dainotti <adainotti6@burano.cc.gatech.edu>
2022-01-14 00:31:49 -0500

    switched to https

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -32,12 +32,12 @@
  * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
 
-const BASE_URL="http://api.grip.inetintel.cc.gatech.edu:80/json";
+const BASE_URL="https://api.grip.inetintel.cc.gatech.edu:443/json";
 
 const TAGS_URL=`${BASE_URL}/tags`
 const ASNDROP_URL=`${BASE_URL}/asndrop`
 const BLOCKLIST_URL=`${BASE_URL}/blocklist`
 
-const FEEDBACK_URL="http://api.grip.inetintel.cc.gatech.edu/dev/feedback"
+const FEEDBACK_URL="https://api.grip.inetintel.cc.gatech.edu/dev/feedback"
 
 export {BASE_URL, TAGS_URL, ASNDROP_URL, BLOCKLIST_URL, FEEDBACK_URL}
